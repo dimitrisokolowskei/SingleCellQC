53277bae3bbaba7775c20ec9c3de62ef98f47415
Dimitri Sokolowskei <dimitri@unknown106fd9744913.attlocal.net>
2024-04-11 21:24:29 -0500

    code

--- QC_function.R
+++ QC_function.R
@@ -1,32 +1,59 @@
 library(Seurat)
 library(SeuratObject)
 
-
-
 seurat <- readRDS("Zhou_2020.rds")
 View(seurat@meta.data)
 
-class(seurat)
 
-QualityControl <- function(obj) {
-  seurat <- readRDS(obj)
+
+
+QCalc <- function(obj, species = c("Human", "Mouse")) {
   
-  if(class(obj) != "SeuratObject") 
+  data <- readRDS(obj)
+  
+  if(class(data) != "Seurat") 
     stop("Not a seurat object")
-  print("Everything is fine around here...")
+  
+  if(species == "Human") {
+    data$log10GenesPerUMI <- log10(data$nFeature_RNA) / log10(data$nCount_RNA)
+    data$mitoRatio <- PercentageFeatureSet(object = data, pattern = "^MT-")
+    data$mitoRatio <- data@meta.data$mitoRatio / 100
+    data$riboRatio <- PercentageFeatureSet(object = data, pattern = "^RP[SL]")
+    data$riboRatio <- data@meta.data$riboRatio / 100
+  }
+  
+  data$log10GenesPerUMI <- log10(data$nFeature_RNA) / log10(data$nCount_RNA)
+  data$mitoRatio <- PercentageFeatureSet(object = data, pattern = "^Mt-")
+  data$mitoRatio <- data@meta.data$mitoRatio / 100
+  data$riboRatio <- PercentageFeatureSet(object = data, pattern = "^Rp[sl]")
+  data$riboRatio <- data@meta.data$riboRatio / 100
+  
+  data
+}  
+
+
+x <- QCalc("Zhou_2020.rds", species = "Mouse")
+View(x@meta.data)
+
+QC <- function(obj, annot = F) {
+  data <- readRDS(obj)
+  
+  if(class(data) != "Seurat") 
+    stop("Not a seurat object")
+  
+  if(annot) {
+    
+  }
+  
 }
 
   
+y <- TRUE
+
+if(y) {
+  print("Hi")
+}
 
 
-
-data <- QualityControl("Zhou_2020.rds")
-
-d <- class(seurat)
-View(class(seurat))
-
-
-is.Seurat(seurat)
-
-is.
-SeuratObject::Seurat-
+x <- QualityControl("Zhou_2020.rds")
+View(x@meta.data)
